4aebb53915775ce7729aa0389e8d8fbe1444687e
juhokarppinen <juho.karppinen@gmail.com>
2017-12-18 15:07:16 +0200

    First prototype.

--- Main.hs
+++ Main.hs
@@ -1,5 +1,51 @@
 module Main where
 
 import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Game
 
-main = display (InWindow "Nice Window" (200, 200) (10, 10)) white (Circle 80)
+data World = World
+    {
+        ballInstance :: Object
+    } deriving Show
+
+
+data Object = Object
+    {
+        loc :: (Float,Float),
+        vel :: (Float,Float),
+        acc :: (Float,Float),
+        pic :: Picture
+    } deriving Show
+
+
+initialState :: World
+initialState = World (
+    Object 
+        (0,0) 
+        (0,0) 
+        (0,0) 
+        (circleSolid 10))
+
+
+render :: World -> Picture
+render w = let ball = ballInstance w in
+    uncurry translate (loc ball) $ pic ball 
+
+
+handleInput :: Event -> World -> World
+handleInput e w = w 
+
+
+iterateState :: Float -> World -> World
+iterateState f w = w     
+
+
+main :: IO ()
+main = play
+    (InWindow "Bouncing Ball" (640,640) (10,10))
+    white
+    60
+    initialState
+    render
+    handleInput
+    iterateState
