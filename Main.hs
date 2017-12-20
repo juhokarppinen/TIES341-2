4e9f541b27b45732f05b446304208adbb971449f
juhokarppinen <juho.karppinen@gmail.com>
2017-12-20 15:34:04 +0200

    Ball movement, hit walls properly

--- Main.hs
+++ Main.hs
@@ -1,29 +1,55 @@
 module Main where
 
+import Data.Monoid
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
 
 
--- Data type contains the ball's location and velocity.
+-- Data type contains the ball's location and velocity and the state
+-- of the keyboard.
 data World = World
     {
         loc :: Point,
-        vel :: Vector
+        vel :: Vector,
+        key :: KeyboardState
     } deriving Show
 
 
-gravity :: Vector
-gravity = (0, 0)
+data KeyboardState = Keys 
+    {
+        upKey :: Bool,
+        downKey :: Bool,
+        leftKey :: Bool,
+        rightKey :: Bool 
+    } deriving Show
 
+
+-- The amount of gravity imparted to the ball.
+gravity :: Vector
+gravity = (0, -0.5)
+
+
+-- The amount of movement damping.
 damping :: Float
 damping = 0.95
 
+
+-- The amount of velocity addition imparted by keyboard movement.
+effect :: Float
+effect = 1
+
+
+ballSize :: Float
+ballSize = 25
+
+
 ballPic :: Picture
-ballPic = circleSolid 25
+ballPic = circleSolid ballSize
+
 
 -- The initial state of the world.
 initialState :: World
-initialState = World (0,100) (0,0)
+initialState = World (0,0) (0,0) (Keys False False False False)
 
 
 -- Translate the ball according to its location.
@@ -31,29 +57,68 @@ render :: World -> Picture
 render w = uncurry translate (loc w) $ ballPic
 
 
--- Add velocity to the ball based on keyboard input.
+-- Update the keyboard state according to keyboard input.
 handleInput :: Event -> World -> World
-handleInput event world = World (loc world) newVel where
-    newVel = vel world .+ (direction .* impulse) where
-        (direction, impulse) = case event of
-            EventKey (SpecialKey KeyUp) _ _ _ -> ((0,1),1)
-            EventKey (SpecialKey KeyDown) _ _ _ -> ((0,-1),1)
-            EventKey (SpecialKey KeyLeft) _ _ _ -> ((-1,0),1)
-            EventKey (SpecialKey KeyRight) _ _ _ -> ((1,0),1)
-            otherwise -> ((0,0),0)
+handleInput event world = World (loc world) (vel world) keys where
+    keys = case event of
+        EventKey (SpecialKey KeyUp) Down _ _ ->
+            orKeys (key world) (Keys True False False False)
+        EventKey (SpecialKey KeyDown) Down _ _ ->
+            orKeys (key world) (Keys False True False False)
+        EventKey (SpecialKey KeyLeft) Down _ _ ->
+            orKeys (key world) (Keys False False True False)
+        EventKey (SpecialKey KeyRight) Down _ _ ->
+            orKeys (key world) (Keys False False False True)
+        EventKey (SpecialKey KeyUp) Up _ _ ->
+            andKeys (key world) (Keys False True True True)
+        EventKey (SpecialKey KeyDown) Up _ _ ->
+            andKeys (key world) (Keys True False True True)
+        EventKey (SpecialKey KeyLeft) Up _ _ ->
+            andKeys (key world) (Keys True True False True)
+        EventKey (SpecialKey KeyRight) Up _ _ ->
+            andKeys (key world) (Keys True True True False)
+        otherwise -> key world
 
 
--- Update the ball's velocity and location based on its acceleration.
+-- Perform the AND operation for two KeyboardStates.
+andKeys :: KeyboardState -> KeyboardState -> KeyboardState
+andKeys (Keys a1 a2 a3 a4) (Keys b1 b2 b3 b4) =
+    Keys (a1 && b1) (a2 && b2) (a3 && b3) (a4 && b4)
+
+
+-- Perform the OR operation for two KeyboardStates.
+orKeys :: KeyboardState -> KeyboardState -> KeyboardState
+orKeys (Keys a1 a2 a3 a4) (Keys b1 b2 b3 b4) =
+    Keys (a1 || b1) (a2 || b2) (a3 || b3) (a4 || b4)
+
+
+-- Update the ball's velocity based on keyboard input.
+-- Update the ball's location based on it's velocity.
 moveBall :: Float -> World -> World
-moveBall seconds world = World (loc world .+ newVel) newVel where 
+moveBall seconds world = World (loc world .+ newVel) newVel (key world) where 
     newVel
         | x <= leftBoundary || x >= rightBoundary = negX v
         | y <= bottomBoundary || y >= topBoundary = negY v
         | otherwise = v
         where
-            x = fst (loc world)
-            y = snd (loc world)
-            v = ((vel world) .+ gravity) .* damping
+            x
+                | fst (loc world) <= leftBoundary = leftBoundary
+                | fst (loc world) >= rightBoundary = rightBoundary
+                | otherwise = fst (loc world)  
+            y 
+                | snd (loc world) <= bottomBoundary = bottomBoundary
+                | snd (loc world) >= topBoundary = topBoundary
+                | otherwise = snd (loc world)
+            v = ((vel world) .+ gravity .+ impulse) .* damping where
+                impulse = (horizontal, vertical) where
+                    horizontal = case key world of
+                        Keys _ _ True False -> -effect
+                        Keys _ _ False True -> effect
+                        otherwise -> 0
+                    vertical = case key world of
+                        Keys True False _ _ -> effect
+                        Keys False True _ _ -> -effect
+                        otherwise -> 0
 
 
 -- Operators for vector math.
@@ -80,10 +145,10 @@ windowSize = (640, 640)
 windowLocation :: (Int,Int)
 windowLocation = (10,10)
 
-leftBoundary = -0.5 * fromIntegral (fst windowSize)
-rightBoundary = 0.5 * fromIntegral (fst windowSize)
-topBoundary = 0.5 * fromIntegral (snd windowSize)
-bottomBoundary = -0.5 * fromIntegral (snd windowSize)
+leftBoundary = -0.5 * fromIntegral (fst windowSize) + 0.75 * ballSize
+rightBoundary = 0.5 * fromIntegral (fst windowSize) - 0.75 * ballSize
+topBoundary = 0.5 * fromIntegral (snd windowSize) - 0.75 * ballSize
+bottomBoundary = -0.5 * fromIntegral (snd windowSize) + 0.75 * ballSize
 
 -- The entry point of the program.
 main :: IO ()
