9eeb28600f18c1d56e89e4c2911dad39b64ed08a
juhokarppinen <juho.karppinen@gmail.com>
2017-12-21 12:24:53 +0200

    Add documentation

--- Main.hs
+++ Main.hs
@@ -15,6 +15,7 @@ data World = World
     } deriving Show
 
 
+-- The data type which contains the state of the arrow keys.
 data KeyboardState = Keys 
     {
         up :: Bool,
@@ -22,6 +23,20 @@ data KeyboardState = Keys
         left :: Bool,
         right :: Bool 
     } deriving Show
+
+
+-- KeyboardStates corresponding to single arrow keys.
+upKey :: KeyboardState
+upKey = Keys True False False False
+
+downKey :: KeyboardState
+downKey = Keys False True False False
+
+leftKey :: KeyboardState
+leftKey = Keys False False True False
+
+rightKey :: KeyboardState
+rightKey = Keys False False False True
 
 
 -- The amount of gravity imparted to the ball.
@@ -39,10 +54,12 @@ effect :: Float
 effect = 1
 
 
+-- The size of the ball.
 ballSize :: Float
 ballSize = 25
 
 
+-- The picture of the ball.
 ballPic :: Picture
 ballPic = circleSolid ballSize
 
@@ -56,18 +73,6 @@ initialState = World (0,0) (0,0) (Keys False False False False)
 render :: World -> Picture
 render w = uncurry translate (loc w) $ ballPic
 
-
-upKey :: KeyboardState
-upKey = Keys True False False False
-
-downKey :: KeyboardState
-downKey = Keys False True False False
-
-leftKey :: KeyboardState
-leftKey = Keys False False True False
-
-rightKey :: KeyboardState
-rightKey = Keys False False False True
 
 -- Update the keyboard state according to keyboard input.
 handleInput :: Event -> World -> World
@@ -92,22 +97,24 @@ handleInput event world = World (loc world) (vel world) keys where
         otherwise -> key world
 
 
--- Perform the AND operation for two KeyboardStates.
+-- Perform the AND operation on two KeyboardStates.
 andKeys :: KeyboardState -> KeyboardState -> KeyboardState
 andKeys (Keys a1 a2 a3 a4) (Keys b1 b2 b3 b4) =
     Keys (a1 && b1) (a2 && b2) (a3 && b3) (a4 && b4)
 
 
--- Perform the OR operation for two KeyboardStates.
+-- Perform the OR operation on two KeyboardStates.
 orKeys :: KeyboardState -> KeyboardState -> KeyboardState
 orKeys (Keys a1 a2 a3 a4) (Keys b1 b2 b3 b4) =
     Keys (a1 || b1) (a2 || b2) (a3 || b3) (a4 || b4)
 
 
+-- Perform the NOT operation on a KeyboardState.
 negateKey :: KeyboardState -> KeyboardState
 negateKey (Keys a b c d) = Keys (not a) (not b) (not c) (not d)
 
 
+-- Handle ball movement.
 moveBall :: Float -> World -> World
 moveBall seconds world = 
     let
@@ -147,12 +154,12 @@ moveBall seconds world =
     in World newLoc newVel (key world)
 
 
--- Sum of two vectors.
+-- Calculate the sum of two vectors.
 (.+) :: Vector -> Vector -> Vector
 (ax, ay) .+ (bx, by) = (ax + bx, ay + by)
 
 
--- Scale a vector.
+-- Scale a vector by a float.
 (.*) :: Vector -> Float -> Vector
 (x, y) .* s = (x * s, y * s)
 
@@ -176,10 +183,13 @@ windowSize = (640, 640)
 windowLocation :: (Int,Int)
 windowLocation = (10,10)
 
+
+-- The boundaries of ball movement.
 leftBoundary = -0.5 * fromIntegral (fst windowSize) + 0.75 * ballSize
 rightBoundary = 0.5 * fromIntegral (fst windowSize) - 0.75 * ballSize
 topBoundary = 0.5 * fromIntegral (snd windowSize) - 0.75 * ballSize
 bottomBoundary = -0.5 * fromIntegral (snd windowSize) + 0.75 * ballSize
+
 
 -- The entry point of the program.
 main :: IO ()
